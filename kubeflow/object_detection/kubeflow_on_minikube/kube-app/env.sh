00379076c5f26c68d1146b8883327dfe0d907ba5
ishanmadan1996 <ishanmadan1996@gmail.com>
2019-06-16 00:48:55 -0700

    added kubeflow directory

---
+++ kubeflow/object_detection/kubeflow_on_minikube/kube-app/env.sh
@@ -0,0 +1,13 @@
+PLATFORM=minikube
+KUBEFLOW_REPO=/home/ishanmadan1996/kubeflow/examples/object_detection/kubeflow_on_minikube
+KUBEFLOW_VERSION=master
+KUBEFLOW_COMPONENTS='"ambassador","jupyter","notebook-controller","jupyter-web-app","profiles","centraldashboard","tf-job-operator","pytorch-operator","spartakus","argo","pipeline"'
+KUBEFLOW_EXTENDEDINFO=false
+KUBEFLOW_KS_DIR=/home/ishanmadan1996/kubeflow/examples/object_detection/kubeflow_on_minikube/kube-app/ks_app
+KUBEFLOW_DOCKER_REGISTRY=
+DOCKER_REGISTRY_KATIB_NAMESPACE=
+K8S_NAMESPACE=kubeflow
+KUBEFLOW_PLATFORM=minikube
+MOUNT_LOCAL=false
+DEPLOYMENT_NAME=kube-app
+
---
+++ kubeflow/object_detection/kubeflow_on_minikube_0.41/kube-app/env.sh
@@ -0,0 +1,9 @@
+PLATFORM=minikube
+KUBEFLOW_REPO=/home/ishanmadan1996/kubeflow/examples/object_detection/kubeflow_on_minikube_0.41
+KUBEFLOW_VERSION=master
+KUBEFLOW_KS_DIR=/home/ishanmadan1996/kubeflow/examples/object_detection/kubeflow_on_minikube_0.41/kube-app/ks_app
+KUBEFLOW_DOCKER_REGISTRY=
+DOCKER_REGISTRY_KATIB_NAMESPACE=
+K8S_NAMESPACE=kubeflow
+KUBEFLOW_PLATFORM=minikube
+MOUNT_LOCAL=false
